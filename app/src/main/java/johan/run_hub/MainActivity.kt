6161b971f859db3f0e27d996436bf1ab8a2041d5
Johan Muco <johanmuco98@gmail.com>
2020-12-27 17:55:51 +0100

    Initial Commit

---
+++ app/src/main/java/johan/run_hub/MainActivity.kt
@@ -0,0 +1,11 @@
+package johan.run_hub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
