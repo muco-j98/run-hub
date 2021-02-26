a9770ff0ce6310d60a239a1787dcdb0322383f09
Johan Muco <johanmuco98@gmail.com>
2021-02-27 00:28:59 +0200

    Gradient Regression Implemented

--- app/src/main/java/johan/run_hub/algorithms/regression/GradientRegression.kt
+++ app/src/main/java/johan/run_hub/algorithms/regression/GradientRegression.kt
@@ -2,29 +2,33 @@ package johan.run_hub.algorithms.regression
 
 import johan.run_hub.utils.JoinedCalories
 
-class GradientRegression(var joinedCalories: List<JoinedCalories>) {
+class GradientRegression(joinedCalories: List<JoinedCalories>) {
 
     private val xCoordinates = joinedCalories.map { i -> i.caloriesConsumed }
     private val yCoordinates = joinedCalories.map { i -> i.caloriesBurned.toDouble() }
 
     //slope
     private var m: Double = 0.0
+
     //y-intercept
     private var b: Double = 0.0
-    private var learningRate = 0.05
+    private var learningRate = 0.0000001
 
-    fun getMBForLine(xyCoordinates: List<JoinedCalories>): Pair<Double, Double> {
-        for (i in xyCoordinates.indices) {
-            val x = xyCoordinates[i].caloriesConsumed
-            val y = xyCoordinates[i].caloriesBurned
+    fun getMBForLine(): Pair<Double, Double> {
+        for (i in xCoordinates.indices) {
+            val x = xCoordinates[i]
+            val y = yCoordinates[i]
 
             val estimation = m * x + b
             val error = y - estimation
 
-            m += error * x * learningRate
+            m += (error * x) * learningRate
             b += error * learningRate
         }
-        return Pair(m,b)
+        return Pair(m, b)
     }
 
+    private val predictedParameters = getMBForLine()
+
+    fun getYValue(x: Double) = predictedParameters.first * x + predictedParameters.second
 }
--- app/src/main/java/johan/run_hub/ui/RegressionAlgorithmFragment.kt
+++ app/src/main/java/johan/run_hub/ui/RegressionAlgorithmFragment.kt
@@ -11,7 +11,9 @@ import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.*
 import dagger.hilt.android.AndroidEntryPoint
 import johan.run_hub.R
+import johan.run_hub.algorithms.regression.GradientRegression
 import johan.run_hub.algorithms.regression.RegressionModel
+import johan.run_hub.utils.JoinedCalories
 import johan.run_hub.viewmodels.AlgorithmViewModel
 import kotlinx.android.synthetic.main.fragment_regression_algorithm.*
 
@@ -19,18 +21,20 @@ import kotlinx.android.synthetic.main.fragment_regression_algorithm.*
 class RegressionAlgorithmFragment : Fragment(R.layout.fragment_regression_algorithm) {
 
     private val algorithmViewModel: AlgorithmViewModel by viewModels()
+    lateinit var joinedCalories: List<Entry>
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        setupScatterChart()
+        setupRegressionChart()
+        setupGradientRegressionChart()
         subscribeToObservers()
     }
 
     private fun subscribeToObservers() {
         algorithmViewModel.getJoinedCalories().observe(viewLifecycleOwner, Observer {
             it?.let {
-                val joinedCalories = it.map { i -> Entry(i.caloriesConsumed.toFloat(), i.caloriesBurned.toFloat()) }
+                joinedCalories = it.map { i -> Entry(i.caloriesConsumed.toFloat(), i.caloriesBurned.toFloat()) }
                 val scatterDataSet = ScatterDataSet(joinedCalories, "Calories Consumed/Burned").apply {
                     valueTextColor = Color.WHITE
                     color = ContextCompat.getColor(requireContext(), R.color.colorAccent)
@@ -38,8 +42,13 @@ class RegressionAlgorithmFragment : Fragment(R.layout.fragment_regression_algori
                 val scatterData = ScatterData(scatterDataSet)
 
                 val regressionModel = RegressionModel(it)
+                val gradientRegression = GradientRegression(it)
+                gradientRegression.getMBForLine()
 
-                tvModelFit.text = regressionModel.getFitOfTheModel().toString()
+                val modelFitText = regressionModel.getFitOfTheModel() * 100
+                val fitText = String.format("%.1f", modelFitText) + "%"
+
+                tvModelFit.text = fitText
 
                 val smallestPoint = it.minByOrNull { x -> x.caloriesConsumed }
                 val largestPoint = it.maxByOrNull { x -> x.caloriesConsumed }
@@ -51,12 +60,27 @@ class RegressionAlgorithmFragment : Fragment(R.layout.fragment_regression_algori
                     regressionModel.getPredictedYValue(it1)
                 }
 
-                val linePoints = listOf(
-                    smallestYValue?.let { it1 -> Entry(smallestPoint.caloriesConsumed.toFloat(), it1.toFloat()) },
-                    largestYValue?.let { it1 -> Entry(largestPoint.caloriesConsumed.toFloat(), it1.toFloat()) }
+                val regressionLinePoints = listOf(
+                    smallestYValue?.let { it1 -> Entry(smallestPoint.caloriesConsumed.toFloat(),
+                        it1.toFloat()) },
+                    largestYValue?.let { it1 -> Entry(largestPoint.caloriesConsumed.toFloat(),
+                        it1.toFloat()) }
                 )
 
-                val lineDataSet = LineDataSet(linePoints, "Best Fit Line")
+                val gradientLineDataSet = LineDataSet(getGradientRegressionPoints(
+                    smallestPoint!!.caloriesConsumed, largestPoint!!.caloriesConsumed,
+                    gradientRegression), "Gradient Regression Line")
+
+                val gradientLineData = LineData(gradientLineDataSet)
+
+                val gradientData = CombinedData()
+                gradientData.setData(scatterData)
+                gradientData.setData(gradientLineData)
+
+                gradientChart.data = gradientData
+                gradientChart.invalidate()
+
+                val lineDataSet = LineDataSet(regressionLinePoints, "Best Fit Line")
                 val lineData = LineData(lineDataSet)
 
                 val data = CombinedData()
@@ -68,7 +92,47 @@ class RegressionAlgorithmFragment : Fragment(R.layout.fragment_regression_algori
         })
     }
 
-    private fun setupScatterChart() {
+    private fun getGradientRegressionPoints(x1: Double, x2: Double,
+                                            gradientRegression: GradientRegression): List<Entry> {
+        val y1 = gradientRegression.getYValue(x1)
+        val y2 = gradientRegression.getYValue(x2)
+
+        val entry1 = Entry(x1.toFloat(), y1.toFloat())
+        val entry2 = Entry(x2.toFloat(), y2.toFloat())
+
+        return listOf(entry1, entry2)
+    }
+
+    private fun setupGradientRegressionChart() {
+        gradientChart.apply {
+            setTouchEnabled(true)
+            isDragEnabled = true
+        }
+        gradientChart.description.apply {
+            isEnabled = true
+            text = "Calories Consumed"
+            textSize = 13f
+            textColor = Color.BLACK
+        }
+        gradientChart.xAxis.apply {
+            position = XAxis.XAxisPosition.BOTTOM
+            axisLineColor = Color.WHITE
+            textColor = Color.WHITE
+            setDrawAxisLine(true)
+            setDrawGridLines(false)
+        }
+        gradientChart.axisLeft.apply {
+            axisLineColor = Color.WHITE
+            textColor = Color.WHITE
+            setDrawGridLines(false)
+        }
+        gradientChart.axisRight.apply {
+            setDrawLabels(false)
+            setDrawAxisLine(false)
+        }
+    }
+
+    private fun setupRegressionChart() {
         combinedChart.apply {
             setTouchEnabled(false)
         }
